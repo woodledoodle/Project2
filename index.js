0222e07303717e2c37076bd89633c485a4139e0c
Anthony Bui <anthonybui@Anthonys-MacBook-Pro.local>
2019-10-08 17:30:41 -0400

    trying to set up passport

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ const hbs = require("hbs")
 const connection = require("./db/connection")
 const parser = require("body-parser")
 const app = express()
+const methodOverride = require("method-override");
+const path= require("path")
+console.log(__dirname)
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "assets")))
 const UserController = require("./controllers/UserController")
 const LogEntryController = require("./controllers/LogEntryController")
+const LoginController = require("./controllers/LoginController")
 app.use(parser.json())
 app.use(parser.urlencoded({
     extended: true
 }))
+app.use(methodOverride("_method"));
 app.use("/user", UserController)
 app.use("/logentry", LogEntryController)
 
+app.use("/login",LoginController)
 app.get("/", (req,res) => {
-    res.send("hello")
-    console.log(req)
+    res.redirect("/logentry/")
+    
     
 })
 
---
+++ middleware/authentication.js
@@ -0,0 +1,4 @@
+const user = require("/db/models/User")
+module.exports = function(){
+    console.log("authenticating")
+}
